b243423835ec3ea27b87a5ec79dc6aa38637e3cd
Park JongHun <thesn515@gmail.com>
2024-11-05 23:32:09 +0900

    feat: android library setting

--- shared/dummy/build.gradle.kts
+++ shared/dummy/build.gradle.kts
@@ -2,3 +2,19 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
+
+android.namespace = "co.kr.parkjonghun.wihd2025.shared.dummy"
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
--- widget/dummy-impl/build.gradle.kts
+++ widget/dummy-impl/build.gradle.kts
@@ -3,6 +3,22 @@ plugins {
     alias(libs.plugins.kotlin.android)
 }
 
+android.namespace = "co.kr.parkjonghun.wihd2025.widget.dummyimpl"
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
 dependencies {
     implementation(project(":feature:dummy"))
     implementation(project(":entity:dummy"))
